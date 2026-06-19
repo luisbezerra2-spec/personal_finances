2be113f41144ad81a932c6087d1fe88553d0622e
luisbezerra2-spec <luis.bezerra2@estudante.ifms.edu.br>
2026-06-18 21:40:50 -0400

    refactoring: removing unnused files and importing and syncing navigation3 dependencies

--- app/src/main/java/com/example/navegacao/MainActivity.kt
+++ app/src/main/java/com/example/navegacao/MainActivity.kt
@@ -3,22 +3,13 @@ package com.example.navegacao
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import com.example.navegacao.data.AppDatabase
 import com.example.navegacao.navigation.SetupNavigation
-import com.example.navegacao.telas.FinancasViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
-        val database = AppDatabase.getDatabase(applicationContext)
-        val viewModel = FinancasViewModel(database.produtoDao())
-
         setContent {
-            SetupNavigation(viewModel = viewModel)
+            SetupNavigation()
         }
     }
 }
-
